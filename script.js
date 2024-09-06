e4e351128d1ea98b1111b3523688f553acfefab2
raymart-021400 <avancenaraymart62@gmail.com>
2024-09-06 10:37:33 +0800

    added lesson

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstname = "Raymart";
+let lastname = "Avancena";
+
+console.log("Hi I'm " + firstname + " " + lastname + " 2nd year Bsit student" );
+alert("Hi I'm" + firstname + " " + lastname + "2nd year BSIT student" );
+
+document.getElementById("Text").innerHTML= "Hi I'm " + firstname + " " + lastname + " 2nd year BSIT student";
